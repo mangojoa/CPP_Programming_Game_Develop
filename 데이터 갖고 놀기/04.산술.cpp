71acbc7c491132da6ed0a9e238bc40be6964c3bd
mangojoa <sxfc675@gmail.com>
2022-08-27 20:04:32 +0900

    [22.08.27] 비교 연산과 논리 연산

--- 데이터 갖고 놀기/04.산술.cpp
+++ 데이터 갖고 놀기/04.산술.cpp
@@ -4,6 +4,12 @@ using namespace std;
 // 데이터의 연산 => 데이터를 가공하는 방법
 int a, b;
 
+// 비교 연산을 위한 변수
+bool isSame;
+bool isDifferent;
+bool isGreater;
+bool isSmaller;
+
 int main() {
     
     // 대입연산
@@ -25,6 +31,31 @@ int main() {
 
     b = a++;
     b = ++a;
-    
 
+#pragma region 비교연산
+    /* 이는 언제 필요한 것인가 
+    판별이 필요한 상황에 전반적으로 사용된다고 보면 된다.
+    ex) HP가 0이 된 상황 / 버프가 들어온 상황 등
+
+    a == b : a와 b의 값이 같은가?*/
+    isSame = (a == b);
+    
+    // a != b : a와 b의 값이 다른가?
+    isDifferent = (a != b);
+
+    // isSame 과 isDiffernt 는 상반되는 개념이다.
+
+    // a > b : a가 b보다 큰가? / a >= b : a가 b보다 크거나 같은가?
+    isGreater = (a > b);
+
+    // a < b : a가 b보다 작은가? / a <= b : a가 b보다 작거나 같은가?
+    isSmaller = (a < b);
+
+#pragma endregion
+
+#pragma region 논리연산 
+
+
+
+#pragma endregion
 }
