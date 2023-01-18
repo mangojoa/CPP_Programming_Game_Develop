66322b0a94f31facdc2edaebb4282dc16936e92f
mangojoa <sxfc675@gmail.com>
2023-01-18 21:44:48 +0900

    [23.01.18] 문자열 복사 함수와 복사하는 원리를 이해하는 시간

--- 13.연습문제_문자열_01/문자열.cpp
+++ 13.연습문제_문자열_01/문자열.cpp
@@ -15,11 +15,43 @@ int strLen(const char* str) {
 	return count;
 }
 
+#pragma warning(disable: 4996)
+// 3. 문자열 복사 함수
+void strCpy(char* dest, char* src) {
+	
+	/*int count = 0;
+	
+	bool ready = true;
+	
+	while (ready) {
+		dest[count] = src[count];
+		cout << dest[count] << endl;
+		if (src[count] == '\0') ready = false;
+
+		count++;
+	}
+	
+	return dest;
+	*/
+
+	// 포인터를 이용하는 방법
+	while (*src != '\0') {
+
+		// *dest++ = *src++; 으로도 가능하다.
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+}
+
+
 int main() {
 
 	const int BUF_SIZE = 100;
 
 	char a[BUF_SIZE] = "Hello";
+	char b[BUF_SIZE];
 	
 	// 1. a의 사이즈를 측정해보자!
 	int len = sizeof(a);
@@ -32,5 +64,20 @@ int main() {
 
 	int realLen02 = strLen(a);
 	cout << realLen02 << endl; // int strLen(const char* str) 함수의 값을 출력
+
+	// b = a; // <- 배열을 일반적인 변수를 복사하는 것처럼 진행할 순 없다. (메모리의 첫 번째 값을 나타내고 있으며 이는 복사가 안되기 때문이다.)
+
+	/* 문자열을 복사하는 함수 strcpy()
+	이 함수의 경우 두 개의 인자값을 받으며 첫 번째 인자값에 두 번째 인자값을 복사한다는 의미이다.
+	하지만 이는 아~!~~~주 오래된 함수라서 위험성이 매우 높으며 컴파일러에 따라 오류를 출력하는 경우가 있다.
+	그럼에도 불구하고 사용하고 싶다면 #pragma warning(disable: 4996) 이라는 문을 int main() {} 전에 선언하면 사용할 수 있다.
+
+	strcpy_s() 라는 함수도 있다 (strcpy()의 안정성이 높아진 버전의 함수라고 볼 수 있다.) */
+
+	// strcpy()
+	strCpy(b, a);
+
+
+	return 0;
 
 }
