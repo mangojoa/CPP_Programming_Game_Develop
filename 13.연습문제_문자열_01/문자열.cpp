f0244fdb7a8f370c78366854ea068249d2f27c0c
이두산 <sxfc675@gmail.com>
2023-01-22 21:55:08 +0900

    [23.01.22] 문자열을 비교하는 함수 strcmp의 개념 및 사용방법 / 함수 구현 연습문제 진행

--- 13.연습문제_문자열_01/문자열.cpp
+++ 13.연습문제_문자열_01/문자열.cpp
@@ -46,9 +46,51 @@ void strCpy(char* dest, char* src) {
 }
 
 // 4. 문자열 덧붙이기 함수
-void strCat(char* dest, char* src) {
+char* strCat(char* dest, char* src) {
+	// int len = strLen(dest);
 
+	// int i = 0;
+	// while (src[i] != '\0') {
+	// 	dest[len + i] = src[i];
+	// 	i++;
+	// }
+
+	// dest[len + i] = '\0';
+
+	// return dest;
+
+	// pointer 를 이용하는 방법
+	char* ret = dest;
+
+	while(*dest != '\0') { dset++; }
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+
+	return ret;
+	
 }
+
+// 5. 두 문자열을 비교하는 함수
+int* strCmp(char* a, char* b) {
+	int value;
+
+	int i = 0;
+	while (a[i] != '\0' || b[i] != '\0') {
+		if (a[i] > b[i]) return 1;
+		if (a[i] < b[i]) return -1;
+	}
+
+	return 0;
+}
+
+// 6. 문자열을 뒤집는 함수
 
 
 int main() {
@@ -86,11 +128,41 @@ int main() {
 	strCpy(b, a);
 
 	char c[BUF_SIZE] = "Hello";
-	char d[BUF_SIZE] = "Hello";
+	char d[BUF_SIZE] = "World";
 
-	// 문자열을 덧붙이는 함수 strcat(dest, src);
+	/* 문자열을 덧붙이는 함수 strcat(dest, src);
+	src의 문자열을 dest에 붙이는 개념의 함수이다 
+	dest의 마지막 요소인 '\0'에 붙여넣어 이어지게 만드는 함수라고 볼 수 있으며 
+	'\0'의 함수 자체을 새롭게 갱신도 하는 개념이다. */
 	strcat(d, c);
 
+	// 문자열을 덧붙이는 함수 strCat(dest, src);
+	strCat(d, c); 
+
+	/* 문자열 중 같은 요소가 있는지 확인하는 함수 strcmp(a, b);
+	우선적으로 a와 b에는 같은 값이 들어가 있다. 그런데 이를 왜 비교하는거야 ???
+	if (a == b) { logic } 만 하면 간단한 일이 아닌가?
+
+	이를 C를 배우면서 가장 헷갈리게 할 수 있는 부분이라고 할 수 있다.
+	우선 적으로 a와 b는 배열 형태의 문자열이며 a와 b의 자체만으로 메모리의 시작 주소를 나타낸다고 할 수 있다.
+
+	if (a == b) 를 진행하면 각자의 값을 비교하는 것이 아는 메모리 주소를 비교하는 것이 되버려 같지 않다는 결과만 출력하게 될것이 뻔하다.
+	즉, 내용물은 같다할 지라도 저장된 위치가 다르기 때문이라는 것이 크게 작용하는 사례이다.
+
+	그리고 추가적으로 비교하려는 문자열의 길이가 같아야한다는 것이 가장 큰 문제이다.
+	문자열의 길이가 서로 다르다면 값은 당연히 다르다고 나오기 때문이다.
+
+	이는 한글자씩 비교하는 것이 정석으로 통한다. */ 
+	int value = strcmp(a, b);
+	/* ?? 왜 이를 받는 인자값이 int로 받아주는 것인가? strcmp() 함수는 반환값이 int 이기 때문이다.
+
+	왜 int형을 반환해야 하는 원리로 작동하는가? => 이유는 간단하다. 
+	컴퓨터는 문자열을 그대로 저장하지 않고 이를 아스키 코드의 형태로 저장하고 비교하기 때문에 
+	비교값들 끼리 비교했을때, 값의 크기에 따라 숫자를 반환하는 것이 기준으로 되어있어 int 형을 반환하는 것이다. */
+
+	// 두 문자열을 비교하는 함수 strCmp(a, b);
+	int value01 = strCmp(a, b);
+	cout << value01 << endl;
 
 	return 0;
 
