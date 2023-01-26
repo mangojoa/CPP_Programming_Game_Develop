5c289a23c4b67a4234cb7737b577b8a8cd0b02d5
이두산 <sxfc675@gmail.com>
2023-01-26 20:08:26 +0900

    [23.01.26] 달팽이 연습문제 구현부 작성

--- 14.달팽이/달팽이.cpp
+++ 14.달팽이/달팽이.cpp
@@ -8,6 +8,7 @@ const int MAX = 100;
 int board[MAX][MAX] = {};
 int N;
 
+// Board 값 출력
 void PrintBoard() {
 	for (int y = 0; y < N; y++) {
 		for (int x = 0; x < N; x++) {
@@ -17,9 +18,72 @@ void PrintBoard() {
 	}
 }
 
+void printXYway(int x, int y) {
+	cout << "-----------------------" << endl;
+	cout << "Y_way : " << y << " | " << "X_way : " << x << endl; 
+}
+
+// Board 값 설정
+void setBoard(int N) {
+	int number = N;
+	cout << "insert INT : " << number << endl;
+
+	// board에 넣을 값 (++로 진행)
+	int count = 0;
+
+	// x, y 축에 대한 변수값
+	int x_way = 0; // x 축 기준
+	int y_way = 0; // y 축 기준
+
+	printXYway(x_way, y_way);
+
+	for (int x = x_way; x < number; x++) {
+		board[y_way][x] = ++count;
+		if (x == (number-1)) { 
+			x_way = x;
+			y_way++;
+		}
+	}
+
+	printXYway(x_way, y_way);
+
+	for (int y = y_way; y < number; y++) {
+		board[y][x_way] = ++count;
+		if (y == (number-1)) {
+			x_way--;
+			y_way = y;
+		}
+	}
+
+	printXYway(x_way, y_way);
+	
+	for (int x = (x_way+1); x--;) {
+		board[y_way][x] = ++count;
+		if (x == (number-number)) {
+			x_way -= x_way; 
+			y_way--;
+			break;
+		}
+	}
+
+	printXYway(x_way, y_way);
+
+	for (int y = (y_way+1); y--;) {
+		board[y][x_way] = ++count;
+		if (y == number - (number -1)) {
+			x_way = y;
+			y_way = --y;
+		}
+	}
+
+	printXYway(x_way, y_way);
+}
+
 int main() {
 
 	cin >> N;
+
+	setBoard(N);
 
 	PrintBoard();
 
