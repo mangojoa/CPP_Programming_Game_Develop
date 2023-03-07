6774c243ef3c2b731d2fd5ad367c0620ba99ab46
mangojoa <sxfc675@gmail.com>
2023-03-07 23:15:33 +0900

    [23.03.07] 디버깅 문제 1번 풀이 작성

--- Exercise 디버깅 문제풀이/Exercise_1/Knight.cpp
+++ Exercise 디버깅 문제풀이/Exercise_1/Knight.cpp
@@ -8,7 +8,10 @@ Knight::Knight() : _hp(100), _attack(10)
 
 }
 
-Knight::Knight(int hp) : _hp(hp)
+// 이 생성자에는 [_attack] 값이 정상적으로 부여되지 않고 있기에
+// 컴파일러가 메모리 영역에 임시로 넣어둔 값 -> 영역구분을 위해 넣어둔 값을
+// 출력하고 있다. 그렇기에 _attack을 추가하면 해당 현상을 사라지게 된다.
+Knight::Knight(int hp) : _hp(hp), _attack(10)
 {
 
 }
