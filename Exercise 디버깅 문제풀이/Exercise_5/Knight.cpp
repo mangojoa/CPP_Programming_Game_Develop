65232a148869cc6be6190d0e99153e4d354825c4
mangojoa <sxfc675@gmail.com>
2023-03-11 20:48:44 +0900

    [23.03.11] 디버깅 문제 5번 풀이 작성

--- Exercise 디버깅 문제풀이/Exercise_5/Knight.cpp
+++ Exercise 디버깅 문제풀이/Exercise_5/Knight.cpp
@@ -32,14 +32,23 @@ bool Knight::IsDead()
 	return (_hp <= 0);
 }
 
+/* [풀이] 0xC0000094: Integer division by zero. 해당 부분에서 문제가 발생하고 있다. */
 int Knight::GetAttackDamage()
 {
 	// hp 50% 이하 => maxHp / hp가 2 이상
 	int damage = _attack;
 
-	int ratio = _maxHp / _hp;
-	if (ratio > 2)
+	if (_maxHp == 0)return damage;
+	int percentage = (_hp / _maxHp) * 100;
+	
+	if (percentage <= 50) {
 		damage *= 2;
+	}
+
+	// int ratio = _maxHp / _hp; // _hp값이 /를 진행할 당시 0이라는 값으로 진행이 되는 것을 확인
+	// 나누기를 진행할 때, 0을 나눈다는 것은 사사리상 말이 안되는 소리이다.	
+	//if (ratio > 2)
+	//	damage *= 2;
 
 	return damage;
 }
