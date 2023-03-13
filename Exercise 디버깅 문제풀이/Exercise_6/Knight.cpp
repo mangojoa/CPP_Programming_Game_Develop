b856b5e0fc95d5d8168f35aed4c1f9abad78066f
mangojoa <sxfc675@gmail.com>
2023-03-13 22:52:42 +0900

    [23.03.13] 디버깅 문제 6번 풀이 작성

--- Exercise 디버깅 문제풀이/Exercise_6/Knight.cpp
+++ Exercise 디버깅 문제풀이/Exercise_6/Knight.cpp
@@ -61,6 +61,15 @@ void Knight::OnDamaged(Knight* attacker)
 	int damage = attacker->GetAttackDamage();
 	AddHp(-damage);
 
+	// 이 로직의 문제는 반격을 마무리하는 로직이 없다는 것이다.
+	if (damage == 0) return;
+	if (IsDead()) return;
+
 	// 반격!
-	attacker->OnDamaged(this);	
+	attacker->OnDamaged(this);
+
+	/* [풀이]
+	가장 큰 문제는 stack overflow가 발생한다는 것인데 
+	함수가 제귀함수(자기 자신을 호출하는 구조)로 되어있다면 지속적으로 
+	스택 영역의 메모리를 사용하게 되므로 프로그램이 뻗어버리게 된다는 것이다. */
 }
