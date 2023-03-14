5d5e201b1f32d3bc7a14929cda32278f2a732ede
mangojoa <sxfc675@gmail.com>
2023-03-15 00:24:19 +0900

    [23.03.14] 디버깅 문제 7번 풀이 작성

--- Exercise 디버깅 문제풀이/Exercise_7/Archer.cpp
+++ Exercise 디버깅 문제풀이/Exercise_7/Archer.cpp
@@ -13,6 +13,9 @@ Archer::Archer(int hp) : Player(hp)
 	_pet = new Pet();
 }
 
+/* [풀이] 
+최상위 클래스에 virtual 함수를 붙였기 때문에 이제 delete 과정에서 
+Archer::~Archer() {} 함수로 넘어오는 것을 확인 할 수 있다. */
 Archer::~Archer()
 {
 	// 즐거웠다 내 펫 :(
