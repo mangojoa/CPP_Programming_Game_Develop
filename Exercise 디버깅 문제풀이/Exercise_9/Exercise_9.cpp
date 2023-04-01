998ec7ae4608141dae3e16d976d2cce9137e5af8
mangojoa <sxfc675@gmail.com>
2023-04-01 20:33:39 +0900

    [23.04.01] 디버깅 문제 9번 풀이 작성

--- Exercise 디버깅 문제풀이/Exercise_9/Exercise_9.cpp
+++ Exercise 디버깅 문제풀이/Exercise_9/Exercise_9.cpp
@@ -28,5 +28,18 @@ int main()
 	archer->AddHp(-damage);
 
 	delete archer;
+	/* [풀이] _pet이 두번 delete가 진행되고 있는 상황이라는 것이다.
+	상황을 살펴본다면
+	
+	AddHp 에서 delete _pet; 에서 진행되고 있다.
+
+	하지만 delete Archer; 를 진행하게 되면 안에 _pet != nullptr 이라면 
+	delete _pet; 을 진행하도록 코드가 설계되어있는데 이는 _pet에 올바르지 못한 메모리 영역값을 가지고 있어
+
+	이를 delete 하려는 시도 때문에 에러가 나는 것을 확인 할 수 있다.
+
+
+	꼭 해당 포인터의 데이터를 삭제하고 나서 nullptr을 상황에 맞게 지정해줘야 문제를 줄일 수 있다.
+	*/
 	delete knight;
 }
