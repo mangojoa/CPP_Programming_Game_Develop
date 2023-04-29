4bfef86937e27a39fce71380b8b6bbaf8129014a
mangojoa <sxfc675@gmail.com>
2023-04-29 17:05:44 +0900

    [23.04.29] vector 에 대하여

---
+++ 01.벡터와STL/백터.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/* STL (Standard Template Library)
+프로그래밍할 때 필요한 자료구조/알고리즘들을 템플릿으로 제공하는 라이브러리
+
+컨테이너 (container) 
+데이터를 저장하는 객체 (자료구조 Data Structure)
+
+vector (동적배열)
+
+*/
+
+int main() {
+
+	/* 배열만으로 게임을 만들기 어려운 이유 (vector 가 필요한 이유)
+	
+	*/
+
+	return 0;
+}
