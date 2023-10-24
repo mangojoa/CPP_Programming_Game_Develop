136075fc171d909b2bbf360bc3640b980bf13344
mangojoa <sxfc675@gmail.com>
2023-10-24 20:48:41 +0900

    [23.10.16] nullptr 의 활용

--- 03.nullptr/nullptr.cpp
+++ 03.nullptr/nullptr.cpp
@@ -7,45 +7,36 @@ using namespace std;
 #include <set>
 #include <algorithm>
 
+/* using 에 대하여
+C++ 11 에서 typedef 을 대처해서 사용할 수 있는 문법이다. */
 
-	/* NULL과 0 의 표현
-	* 없다는 것을 표현하기 위해서는 0 아니면 null 과 같은 식으로 사용했다.
-	*
-	* 하지만 이렇게 사용하면 문제가 있다.
-	*
-	* 우리가 어떤 포인터를 표현하고 싶은데
-	* 포인터 자체가 어떤 값도 없을때는 사실상 0이라는 값이 들어가는 것은 맞다.
-	*
-	* 하지만 타고갈 주소가 없음을 나타내고 있는 값은 0으로 나타난다. */
-	int* ptr = NULL; // 0 NULL
+// 기존의 typedef을 이용한 선언
+typedef vector<int>::iterator vecr;
+typedef __int64 id;
 
-	/* NULL를 타고 들어가면 #define NULL 0 이라는 것을 확인할 수 있다.
-	 NULL이라는 값은 이 변수가 포인터라는 힌트를 프로그램한테 주기 위한 용도이지
-	 사실상 ptr 이라는 변수를 '0'이라는 값으로 놓은 것과 별반 다를바가 없다.
+// 하지만 using 을 사용하는 방법에 대해 알아보자.
+using id2 = int;
 
-	 NULL = 0 이라는 것이다! */
+// 1) 직관성
+typedef void(*MyFunc)();
+using MyFunc2 = void(*)(); // <- typedef으로 작성한 코드에 비해
+// 훨씬 가독성이 현대에 사용되는 코드스타일과 맞게 변화함을 알수 있다.
 
-void Test(int a) {
-	cout << "Test(int)" << endl;
-}
+// 2) 템플릿
+// template<typename T>
+// typedef void // C++925 Error 가 발생하며
+// 템플릿은 기본적으로 typedef과 같이 사용할 수 없다.
+// using 이 사용성에 있어 매우 넓고 간단하게 사용할 수 있기에
+// C++ 11이후에는 using 을 더 많이 사용하긴 한다.
 
-void Test(void* ptr) {
-	cout << "Test(*)" << endl;
-}
-/* 위의 두 개의 함수가 있다고 가정하자.
-각 인자값에 따라 다른 로직이 실행되는 양상으로 보여진다.
-
-하지만 결과값은 상당히 충격적이다. */
-
+template<typename A>
+using List = std::list<A>;
 
 int main() {
-	int* ptr = NULL; // 0 NULL
 
-	/* 위의 각각의 함수를 실행시킬려고 했을 때, 
-	
-	*/
-	Test(0);
-	Test(NULL);
+    id playerId = 0;
 
-	return 0;
+    List<int> li;
+
+    return 0;
 }
